bda94617aa283587cd3164a92a6586dd58d37e5d
VladMursalimov <74963780+VladMursalimov@users.noreply.github.com>
2020-11-24 17:27:46 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from random import randint
+from PyQt5 import uic
+
+
+class Widget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('ui.ui', self)
+        self.point = [randint(50, 400), randint(50, 400)]
+        self.pushButton.clicked.connect(self.run)
+
+    def run(self):
+        self.point = [randint(50, 400), randint(50, 400)]
+        self.update()
+
+    def paintEvent(self, event):
+        super().paintEvent(event)
+        painter = QPainter(self)
+        painter.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
+        rad = randint(0, 100)
+        painter.drawEllipse(self.point[0], self.point[1], rad, rad)
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    w = Widget()
+    w.show()
+    app.exec()
